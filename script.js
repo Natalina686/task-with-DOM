51b26d439ec09cb71a2852c7071e2dbbcce68569
Natalina686 <108237928+Natalina686@users.noreply.github.com>
2023-06-15 15:17:40 +0200

    html,js

---
+++ script.js
@@ -0,0 +1,14 @@
+"use strict";
+
+
+const navigation = document.querySelector('#headerTwo');
+console.log(navigation);
+
+const firstElement = document.querySelector('.firstSection');
+console.log(firstElement.querySelector("h2"));
+
+ const listItem = document.querySelector("li.text:nth-child(4)");
+console.log(listItem);
+
+const spanWithClass = document.querySelector('.hatredLevelBlock');
+console.log(spanWithClass);
